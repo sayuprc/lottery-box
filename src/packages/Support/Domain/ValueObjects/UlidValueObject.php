81a44016d876ea900be98ce6b098a7ee9fa7b126
sayuprc <bepdielibe+github@gmail.com>
2025-06-07 20:28:47 +0900

    ULID の基底クラスを作成

---
+++ src/packages/Support/Domain/ValueObjects/UlidValueObject.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Support\Domain\ValueObjects;
+
+use Support\Domain\Exceptions\InvalidFormatException;
+
+abstract class UlidValueObject extends StringValueObject
+{
+    public function __construct(string $value)
+    {
+        if (! preg_match('/\A[0-9a-hjkmnp-zA-HJKMNP-Z]{26}\z/', $value)) {
+            throw new InvalidFormatException('Invalid ULID format.');
+        }
+
+        parent::__construct($value);
+    }
+}
