0840790e4caca54cc55d2d4283b40473a36ab359
sayuprc <bepdielibe+github@gmail.com>
2025-06-08 12:34:47 +0900

    ID を ULID に変更

--- src/packages/Lottery/Domain/Models/LotteryItem/ItemId.php
+++ src/packages/Lottery/Domain/Models/LotteryItem/ItemId.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Lottery\Domain\Models\LotteryItem;
 
-use Support\Domain\ValueObjects\StringValueObject;
+use Support\Domain\ValueObjects\UlidValueObject;
 
-class ItemId extends StringValueObject
+class ItemId extends UlidValueObject
 {
 }
