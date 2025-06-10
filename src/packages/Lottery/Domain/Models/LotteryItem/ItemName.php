85d1effe1b07486c5ce8987c4374edc9d97c8b24
sayuprc <bepdielibe+github@gmail.com>
2025-06-11 00:28:56 +0900

    抽選アイテムのバリデーションを追加

--- src/packages/Lottery/Domain/Models/LotteryItem/ItemName.php
+++ src/packages/Lottery/Domain/Models/LotteryItem/ItemName.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace Lottery\Domain\Models\LotteryItem;
 
+use Support\Domain\Exceptions\InvalidValueException;
 use Support\Domain\ValueObjects\StringValueObject;
 
 class ItemName extends StringValueObject
 {
+    public function __construct(string $value)
+    {
+        $value = mb_trim($value);
+
+        if ($value === '') {
+            throw new InvalidValueException('無効な抽選アイテム名です。');
+        }
+
+        parent::__construct($value);
+    }
 }
